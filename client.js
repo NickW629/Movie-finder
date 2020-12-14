438602220e6ef0cae43c0d4b5b02512a0efde4c1
NickW629 <56590336+NickW629@users.noreply.github.com>
2020-12-14 13:31:24 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,35 @@
-const data = loadData();
-function loadData() {
+const comedyData = loadComedy();
+function loadComedy() {
+  const movies = [
+    {
+      name: "",
+      genre:"" ,
+      
+    }
+   ]
+  }
+const horrorData = loadHorror();
+function loadHorror() {
+  const movies = [
+    {
+      name: "",
+      genre:"" ,
+      
+    }
+   ]
+  }
+const dramaData = loadDrama();
+function loadDrama() {
+  const movies = [
+    {
+      name: "",
+      genre:"" ,
+      
+    }
+   ]
+  }
+const scifiData = loadScifi();
+function loadScifi() {
   const movies = [
     {
       name: "",
